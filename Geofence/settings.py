cc65024c8b45bf3382c21aad859b006076db04e6
abhinavanand500 <abhinav.anand500@gmail.com>
2020-06-10 18:55:46 +0530

    templates and static issue

--- Geofence/settings.py
+++ Geofence/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Geofence.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates']
+        'DIRS': ['mysite/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/mysite/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "mysite/static"),
 ]
