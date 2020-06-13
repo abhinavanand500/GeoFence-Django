b2592e842c514fd6f9c9710bd8bb93e29e535368
abhinavanand500 <abhinav.anand500@gmail.com>
2020-06-13 19:19:15 +0530

    Static loc

--- mysite/views.py
+++ mysite/views.py
@@ -7,13 +7,9 @@ from math import sqrt
 from datetime import datetime
 from django.http import JsonResponse
 import json
-
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
-
-
-
 def signup(request):
     if (request.method == 'POST') :
         name = request.POST['name']
@@ -31,61 +27,66 @@ def signup(request):
         else:
             messages.warning(request, "Your admin Password is not correct. This field is strictly for admin")
     return render(request, 'contact.html')
-
 @csrf_exempt
 def attendance(request):
     print("hii")
     content = json.loads(request.body)
     print(content)
+    now = datetime.now()
+    time1 = now.strftime("%H:%M:%S")
     var_date = datetime.today().strftime('%Y-%m-%d')
 
     # Emulator location
-    long_coll = 37.421998333333335
-    lat_coll = -122.08400000000002
+    lat_coll = 37.421998333333335
+    long_coll = -122.08400000000002
 
     # college location
-    # long_coll=12.9337
-    # lat_coll=77.6921
+    # lat_coll=12.9337
+    # long_coll=77.6921
 
     # home location
-    # long_coll = 23.35672372
-    # lat_coll = 85.30982892
-
+    # lat_coll = 23.35672372
+    # long_coll = 85.30982892
+    uq = content['uq']
+    print("This is uq :: ", uq)
     usn = content['usn']
     password1 = content['password']
     mac1 = content['mac']
     long = content['long']
     lat = content['lat']
-    long1 = float(long)
-    lat1 = float(lat)
-    s = mac1.keys()
-    mac2 = '12'
-    for i in s :
-        mac2 = mac2 + "." + i[1:]
+    if(usn=='' and password1==''):
+        data = {'message' : 'Your Unique Id is :: '+ uq}
+        return JsonResponse(data)
+    # s = mac1.keys()
+    # mac2 = '12'
+    # for i in s :
+    #     mac2 = mac2 + "." + i[1:]
+    print("Hello You are coming out of area")
     db = Student.objects.filter(usn=usn, password=password1)
     all = Attendance.objects.filter(date=var_date,usn=usn)
     if(db):
         dist = sqrt((long_coll - long) ** 2 + (lat_coll - lat) ** 2)
         if(dist<10):
             if(all.first()==None):
-                today = Attendance(mac=mac2, usn=usn)
+                today = Attendance(mac=uq, usn=usn, time=time1, date=var_date)
                 today.save()
-                data = {'message' : 'Marked Your Attendance', 'code' : 'SUCCESS'}
+                data = {'message' : 'Marked Your Attendance'}
                 return JsonResponse(data)
             else:
-                data = {'message' : 'Your Attendance is already marked for today', 'code' : 'SUCCESS'}
+                data = {'message' : 'Your Attendance is already marked for today'}
                 return JsonResponse(data)
         else:
-            data = {'message' : 'You are outside Geofencing location', 'code' : 'FAILED'}
+            data = {'message' : 'You are outside Geofencing location'}
             return JsonResponse(data)
     else:
-        data = {'message' : 'Invalid Username and Password', 'code' : 'FAILED'}
+        data = {'message' : 'Invalid Username and Password'}
         return JsonResponse(data)
 
 def display(request):
     var_date = datetime.today().strftime('%Y-%m-%d')
     db1 = Student.objects.values()
     all = Attendance.objects.filter(date=var_date)
+    print(all.values())
     presentcount = len(all)
     totalcount = len(db1)
     params = {'object':all , 'presentCount': presentcount, 'totalCount':totalcount}
